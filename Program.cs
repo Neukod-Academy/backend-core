1aaad09d0699d30a2e819f7eda04936a8bdd925d
noctisnyxx <dinar.rahmat5@gmail.com>
2025-08-21 15:36:43 +0700

    chore: first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "NeukodCoreAPI";
+    config.Title = "Neukod v0";
+    config.Version = "v0";
+});
+
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "NeukodCoreAPI";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapGet("/", () =>
+{
+    return "hello from neukod backend core!";
+}).WithName("HelloApi");
+
+
+app.Run();
